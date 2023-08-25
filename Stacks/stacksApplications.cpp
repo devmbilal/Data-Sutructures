f0643912480b07d50672478a2405ca376eb6736a
Bilal Shakeel <info.bilalshakeel@gmail.com>
2023-08-25 23:21:15 +0500

    post converter aadded

---
+++ Stacks/stacksApplications.cpp
@@ -0,0 +1,64 @@
+string postconvertor(string input)
+{
+	Stack s;
+	string output;
+	int i = 0;
+	try
+	{
+		while (input[i])
+		{
+			if (checkinput(input[i]))
+			{
+				if (s.empty())
+				{
+					s.push(input[i]);
+					i++;
+					continue;
+				}
+				if (input[i] == '(')
+				{
+					s.push(input[i]);
+					i++;
+					continue;
+				}
+				if (input[i] == ')')
+				{
+					while (s.top() != '(')
+					{
+						output.push_back(s.top());
+						s.pop();
+					}
+					s.pop();
+					i++;
+					continue;
+				}
+				if (prec(input[i]) <= prec(s.top()) && s.top() != '(')
+				{
+					output.push_back(s.top());
+					s.pop();
+					s.push(input[i]);
+				}
+				else
+				{
+					s.push(input[i]);
+				}
+
+			}
+			else
+			{
+				output.push_back(input[i]);
+			}
+			i++;
+		}
+		while (!s.empty())
+		{
+			output.push_back(s.top());
+			s.pop();
+		}
+	}
+	catch (const char*msg)
+	{
+		cout << msg << endl;
+	}
+	return output;
+}	
